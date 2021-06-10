1165ee0c02786af29afb3d138c65333c49725133
Em Orendorff <em.orendorff@gmail.com>
2021-06-09 20:03:10 -0500

    Add nav and sections

--- src/fetch-calls.js
+++ src/fetch-calls.js
@@ -1,6 +1,6 @@
-getAllUserData () => {
+getGuestData () => {
     return fetch('http://localhost:3001/api/v1/customers')
     .then(response => response.json())
-    .then(data => data.users)
+    .then(data => data.customers)
     .catch(console.log('error'))
 }
