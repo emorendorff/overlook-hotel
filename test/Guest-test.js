be5455adfc6b03e0a21c8aca2d3784cb8eed38d1
Em Orendorff <em.orendorff@gmail.com>
2021-06-10 14:56:33 -0500

    Add sample data to User-test

---
+++ test/Guest-test.js
@@ -0,0 +1,29 @@
+import { expect } from 'chai'
+import Guest from '../src/Guest.js'
+
+describe('Guest', () => {
+  let guest 
+  let guest2 
+  let guest3
+
+  beforeEach(() => {
+    users = [ 
+      {
+        "id": 1,
+        "name": "Leatha Ullrich"
+      },
+      {
+        "id": 2,
+        "name": "Rocio Schuster"
+      },
+      {
+        "id": 3,
+        "name": "Kelvin Schiller"
+      },
+      {
+        "id": 4,
+        "name": "Kennedi Emard"
+      }
+    ];
+  })
+})
