90fa2ab2ce7d02e3c0920acd442076172f65062d
Em Orendorff <em.orendorff@gmail.com>
2021-06-11 11:11:29 -0500

    Add api calls for the all data in rooms, customers and bookings

--- src/fetch-calls.js
+++ src/fetch-calls.js
@@ -1,6 +1,19 @@
-getGuestData () => {
+import { makeFetchData, onLoad } from './scripts'
+
+fetchGuestData = () => {
     return fetch('http://localhost:3001/api/v1/customers')
     .then(response => response.json())
-    .then(data => data.customers)
-    .catch(console.log('error'))
+    .catch(console.error('customer api error');
+};
+
+fetchRoomsData = () => {
+    return fetch('http://localhost:3001/api/v1/rooms')
+    .then(response => response.json())
+    .catch(console.error('room api error'))
+};
+
+fetchBookingsData = () => {
+    return fetch('	http://localhost:3001/api/v1/bookings')
+    .then(response => response.json())
+    .catch(console.error('all bookings api error'))
 }
