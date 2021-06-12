ccd3ee55d40e1cf4dfaae7e5ff6122392e77b4d5
Em Orendorff <em.orendorff@gmail.com>
2021-06-12 10:46:40 -0500

    Add variables and normalize to scss

--- src/fetch-calls.js
+++ src/fetch-calls.js
@@ -1,33 +1,33 @@
-import { makeFetchData, onLoad } from './scripts'
+// import { makeFetchData, onLoad } from './scripts'
 
-const fetchGuestData = () => {
-    return fetch('http://localhost:3001/api/v1/customers')
-    .then(response => response.json())
-    .catch(console.error('customer api error')
-};
+// const fetchGuestData = () => {
+//     return fetch('http://localhost:3001/api/v1/customers')
+//     .then(response => response.json())
+//     .catch(console.error('customer api error')
+// };
 
-const fetchRoomsData = () => {
-    return fetch('http://localhost:3001/api/v1/rooms')
-    .then(response => response.json())
-    .catch(console.error('room api error'));
-};
+// const fetchRoomsData = () => {
+//     return fetch('http://localhost:3001/api/v1/rooms')
+//     .then(response => response.json())
+//     .catch(console.error('room api error'));
+// };
 
-const fetchBookingsData = () => {
-    return fetch('	http://localhost:3001/api/v1/bookings')
-    .then(response => response.json())
-    .catch(console.error('all bookings api error'));
-};
+// const fetchBookingsData = () => {
+//     return fetch('	http://localhost:3001/api/v1/bookings')
+//     .then(response => response.json())
+//     .catch(console.error('all bookings api error'));
+// };
 
-const fetchAllData = () => {
-    Promise.all([fetchGuestData(), fetchRoomsData(), fetchBookingsData()])
-    .then(data => makeFetchData(data))
-    .then(() => onLoad());
-};
+// const fetchAllData = () => {
+//     Promise.all([fetchGuestData(), fetchRoomsData(), fetchBookingsData()])
+//     .then(data => makeFetchData(data))
+//     .then(() => onLoad());
+// };
 
-export default {
-    fetchAllData,
-    fetchBookingsData,
-    fetchRoomsData,
-    fetchGuestData
-};
+// export default {
+//     fetchAllData,
+//     fetchBookingsData,
+//     fetchRoomsData,
+//     fetchGuestData
+// };
 
