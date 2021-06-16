0f105e196d8d12cbd49d3451d4c15bd514f9514e
Em Orendorff <em.orendorff@gmail.com>
2021-06-16 12:35:14 -0500

    Readd user class

--- src/User.js
+++ src/User.js
@@ -1,30 +1,24 @@
 class User {
-  constructor(userID) {
-    this.userID = userID;
-    this.password = 'overlook2021';
-    this.isUser = false;
-    this.totalSpent = 0;
-    this.id = 0;
-  }
-
-  validateUser(userID) {
-    if (userID.startsWith('customer')) {
-      this.getIdNumber(userID) 
+    constructor(userID) {
+      this.userID = userID;
+      this.password = 'overlook2021';
+      this.isUser = false;
+      this.totalSpent = 0;
+      this.id = 0;
+    }
+  
+    validateUser(userID) {
+      if (userID.startsWith('customer')) {
+        this.getIdNumber(userID) 
+      }
+    }
+    getIdNumber(userID) {
+      this.id = Number.parseInt(userID.slice(8)) 
+      if (this.id > 0 && this.id <= 50) {
+        this.isUser = true
+      }
     }
   }
-  getIdNumber(userID) {
-    this.id = Number.parseInt(userID.slice(8)) 
-    if (this.id > 0 && this.id <= 50) {
-      this.isUser = true
-    }
-  }
-}
-
-
-
-
-// use startsWith method to verify customer is the first part of user name
-// if so then change isUser to true and then validate number is between 1 and 51
-//maybe put that in a seperate function? 
-//might need to use....slice?
-export default User
+  
+  export default User
+  
